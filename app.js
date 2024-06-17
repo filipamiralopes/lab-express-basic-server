638c2d47695cf42a11867de2695a2cecd17a6301
Filipa <filipamnmlopes@hotmail.com>
2024-06-17 12:47:17 +0200

    it3 - create home route

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(morgan("dev"));
 
 // ROUTES
 // Start defining your routes here:
-
+app.get("/", (req, res)=>{
+    res.status(200).sendFile(__dirname + "/views/home.html")
+})
 
 
 // START THE SERVER
